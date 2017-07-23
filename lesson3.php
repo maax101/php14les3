4b503f70227ec7ac664d007ec0e0bde5061a1aa4
maax101 <belous.maxim@gmail.com>
2017-07-23 16:33:02 +0300

    23.07.17

--- lesson3.php
+++ lesson3.php
@@ -2,32 +2,37 @@
 
 $cont = (
 	array(
-		'Africa' => array('Crocodilia','Hippopotamus amphibius'),
-		'northAm'=> array('Ursus arctos','Procyon lotor'),
-		'southAm'=> array('Lama glama','Folivora'),
-		'Antarct'=> array('Orcinus orca','Ommatophoca rossii'),
-		'Eurasia'=> array( 'Vulpes','Canis lupus')
+		'Africa' => array('Crocodilia','Hippopotamus amphibius',),
+		'North America'=> array('Ursus arctos','Procyon lotor',),
+		'South America'=> array('Lama glama','Folivora',),
+		'Antarctica'=> array('Orcinus orca','Ommatophoca rossii',),
+		'Eurasia'=> array( 'Vulpes','Canis lupus',)
 	)
+	//создан массив континетов и соотв зверей
 );
 foreach ($cont as $k => $v) {
 	foreach ($v as $all) {
-		if (strpos ($all,' ')) {
-		 	$double = $all;
-		 	$arr_d = explode(' ', $double);
-		 	$arr_first = array($arr_d [0] );
-			$arr_second = array($arr_d [1]);
-			
-	 		$fant = array_merge($arr_first, $arr_second );
-	 			 		
-			echo '<pre>';
-			print_r($fant) ;
-
-
-		 } 
- 	
-		
-	}	
 	
-}
+		if (strpos ($all,' ')) {  //строка. если есть " "
 
-    
+		 	// массив. присвоить в arr_d значение животное из 2 слов
+		 	//$arr_d [$k][] = explode(' ', $all);
+			$double = explode(' ', $all);
+			//print_r ($double);
+		 	$arr_first[$k][] = $double [0] ;
+			$arr_second[$k][] = $double [1];			
+		} 	
+	}		
+}
+echo '<pre>';
+//print_r($arr_d);
+//print_r ($arr_first);
+shuffle ($arr_second);
+//print_r ($arr_second ) ;
+
+foreach ($arr_first as $k=>$v){
+	echo "<h2>$k</h2> ".PHP_EOL;
+	foreach ($v as $val){
+		echo $val.PHP_EOL;
+	}	
+}
