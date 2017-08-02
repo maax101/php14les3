361f13b6e899232d76437c1f071a50c520f2a431
maax101 <belous.maxim@gmail.com>
2017-08-02 21:55:48 +0300

    final

--- lesson3.php
+++ lesson3.php
@@ -1,38 +1,43 @@
 <?php
-
-$cont = (
-	array(
-		'Africa' => array('Crocodilia','Hippopotamus amphibius',),
-		'North America'=> array('Ursus arctos','Procyon lotor',),
-		'South America'=> array('Lama glama','Folivora',),
-		'Antarctica'=> array('Orcinus orca','Ommatophoca rossii',),
-		'Eurasia'=> array( 'Vulpes','Canis lupus',)
-	)
-	//создан массив континетов и соотв зверей
-);
-foreach ($cont as $k => $v) {
-	foreach ($v as $all) {
-	
-		if (strpos ($all,' ')) {  //строка. если есть " "
-
-		 	// массив. присвоить в arr_d значение животное из 2 слов
-		 	//$arr_d [$k][] = explode(' ', $all);
-			$double = explode(' ', $all);
-			//print_r ($double);
-		 	$arr_first[$k][] = $double [0] ;
-			$arr_second[$k][] = $double [1];			
-		} 	
-	}		
-}
-echo '<pre>';
-//print_r($arr_d);
-//print_r ($arr_first);
-shuffle ($arr_second);
-//print_r ($arr_second ) ;
-
-foreach ($arr_first as $k=>$v){
-	echo "<h2>$k</h2> ".PHP_EOL;
-	foreach ($v as $val){
-		echo $val.PHP_EOL;
+$cont=[	'Africa' 		=> ['Crocodilia godzilia','Hippopotamus amphibius',],
+		'North America'	=> ['Ursus arctos horribilis','Procyon lotor',],
+		'South America'	=> ['Lama glama','Folivora',],
+		'Antarctica'	=> ['Orcinus orca','Ommatophoca rossii',],
+		'Eurasia'		=> ['Vulpes shmulpes','Canis lupus',]
+];
+$arr_first = array();
+$arr_second = array();
+$shuf=array();
+foreach ($cont as $k=> $v):
+	foreach ($v as $i) {
+		if (str_word_count($i)==2) {         //найдены названия из 2 слов
+			$double = explode(' ', $i);	
+			$arr_first[$k][] = $double [0] ;
+			$arr_second[] = $double [1];
+		}
 	}	
+endforeach;
+shuffle($arr_second);
+$z=0;
+foreach ($arr_first as $k => $v) {
+
+	foreach ($v as $value) {
+		
+		$shuf[$k][]=$value.' '.$arr_second[$z];
+		$z++;
+
+	}	
+}
+
+foreach ($shuf as $k => $v) {
+	echo "<h2>$k</h2> ".PHP_EOL;
+	foreach ($v as $val) {
+		
+			if (next($v)) {			//ставим зпт, если есть еще тварь
+				echo $val.', ';
+			}else {
+				echo $val.'.';
+			}
+		
+	}
 }
