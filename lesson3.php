d1646e511554ca49913613f045c80614919b734f
maax101 <belous.maxim@gmail.com>
2017-08-04 16:36:44 +0300

    Add files via upload

--- lesson3.php
+++ lesson3.php
@@ -1,37 +1,30 @@
 <?php
-$animals = ['Africa' => ['Crocodilia godzilia', 'Hippopotamus amphibius',],
+$all_animals = ['Africa' => ['Crocodilia godzilia', 'Hippopotamus amphibius',],
     'North America' => ['Ursus arctos horribilis', 'Procyon lotor',],
     'South America' => ['Lama glama', 'Folivora',],
     'Antarctica' => ['Orcinus orca', 'Ommatophoca rossii',],
-    'Eurasia' => ['Vulpes shmulpes', 'Canis lupus',]
+    'Eurasia' => ['Vulpes shmulpes', 'Canis lupus',],
 ];
-$arr_first = array();
-$arr_second = array();
-$fant_an = array();
-foreach ($animals as $cont => $animal) {
-    foreach ($animal as $anim) {
-        if (str_word_count($anim) == 2) {         //найдены названия из 2 слов
-            $double = explode(' ', $anim);
-            $arr_first[$cont][] = $double [0];
-            $arr_second[] = $double [1];
+$first_word = array();
+$second_word = array();
+$fantastic_animals = array();
+foreach ($all_animals as $continent => $animals) {
+    foreach ($animals as $animal) {
+        if (str_word_count($animal) == 2) {         //найдены названия из 2 слов
+            $double = explode(' ', $animal);
+            $first_word[$continent][] = $double [0];
+            $second_word[] = $double [1];
         }
     }
 }
-shuffle($arr_second);
+shuffle($second_word);
 $i = 0;
-foreach ($arr_first as $cont => $first) {
+foreach ($first_word as $continent => $first) {
     foreach ($first as $value) {
-        $fant_an[$cont][] = $value . ' ' . $arr_second[$i];
+        $fantastic_animals[$continent][] = $value . ' ' . $second_word[$i];
         $i++;
     }
 }
-foreach ($fant_an as $cont => $monstr) {
-    echo "<h2>$cont</h2> " . PHP_EOL;
-    foreach ($monstr as $val) {
-        if (next($monstr)) {            //ставим зпт, если есть еще тварь
-            echo $val . ', ';
-        } else {
-            echo $val . '.';
-        }
-    }
+foreach ($fantastic_animals as $continent => $monster) {
+    echo "<h2>$continent</h2> " . PHP_EOL.implode(', ', $monster). PHP_EOL;
 }
